e52d117dedb85adbf98ea896e8a21f71127e1ba8
Mahbubur Rahman <rifat0153@gmail.com>
2021-03-30 21:33:04 +0600

    font and color added

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.purple,
@@ -18,7 +19,9 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Lato',
       ),
       home: ProductsOverview(),
-      getPages: [GetPage(name: '/home', page: () => HomePage())],
+      getPages: [
+        GetPage(name: '/home', page: () => HomePage()),
+      ],
     );
   }
 }
